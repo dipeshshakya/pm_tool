5ca8a088353e38443f805371ba29a61f97f6daeb
dipesh shakya <dipeshshakya106@gmail.com>
2020-09-13 15:49:57 +1000

    landing page banner setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import { Container } from "@material-ui/core";
+import Home from "./pages/Home";
 
 function App() {
-  return <Header />;
+  return (
+    <Container maxWidth="false" disableGutters="false">
+      <Header />
+      <Home />
+    </Container>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,48 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import { Typography } from "@material-ui/core";
+import { Container } from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  btn: {
+    marginTop: "20px",
+  },
+  containerWrapper: {
+    paddingTop: "50px",
+  },
+}));
+function Home() {
+  const classes = useStyles();
+
+  return (
+    <Container>
+      <Grid
+        className={classes.containerWrapper}
+        container
+        direction="row"
+        alignItems="center"
+        alignItems="center"
+      >
+        <Grid item xs={12} sm={6}>
+          <Typography variant="h2" component="h2">
+            Manage your project from the multiple platform{" "}
+          </Typography>
+          <Button className={classes.btn} variant="outlined" color="secondary">
+            Get Started
+          </Button>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <img
+            src="/images/design-driven-illustration-2x.png"
+            alt="pm"
+            width="550"
+          />
+        </Grid>
+      </Grid>
+    </Container>
+  );
+}
+
+export default Home;
